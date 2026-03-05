43ca503e3e395373cfc9a4f03623a9abad92479a
EnderKaran <ender.karan14@gmail.com>
2026-03-05 10:54:45 +0300

    yeni özellikler eklendi

--- ek-test-utils/src/index.ts
+++ ek-test-utils/src/index.ts
@@ -1,19 +1,39 @@
 import { ReactElement } from 'react';
-import { render, RenderOptions } from '@testing-library/react';
+import { render, renderHook, RenderOptions, RenderHookOptions } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-const AllTheProviders = ({ children}: { children: React.ReactNode }) => {
-    return children;
-}
-
-//Render Fonksiyonu
-const customRender = (
-    ui: ReactElement,
-    options?: Omit<RenderOptions, 'wrapper'>
-) => {
-    return render(ui , { wrapper: AllTheProviders, ...options });
+const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
+  return children; 
 };
 
+const customRender = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, 'wrapper'>
+) => {
+  return render(ui, { wrapper: AllTheProviders, ...options });
+};
+
+const customRenderHook = <Result, Props>(
+  hook: (initialProps: Props) => Result,
+  options?: Omit<RenderHookOptions<Props>, 'wrapper'>
+) => {
+  return renderHook(hook, { wrapper: AllTheProviders, ...options });
+};
+
+export const mockNextNavigation = () => ({
+  useRouter: () => ({
+    push: () => {},
+    replace: () => {},
+    prefetch: () => {},
+    back: () => {},
+    refresh: () => {},
+  }),
+  usePathname: () => '/',
+  useSearchParams: () => new URLSearchParams(),
+});
+
 export * from '@testing-library/react';
+
 export { customRender as render };
+export { customRenderHook as renderHook };
 export { userEvent };
